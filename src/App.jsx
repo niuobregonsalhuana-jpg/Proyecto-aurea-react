1001381dcb015862d8fd55b8f768e63b92981242
niki <nikmendozayarleque@crackthecode.la>
2025-10-17 10:46:33 -0500

    carrusel

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,33 @@ import Hero from './components/Hero'
 // import MascotaPchan from './components/MascotaPchan'
 import Footer from './components/Footer'
 import SobreAurea from './pages/SobreAurea'
+import Carousel from './components/Carrusel'
 
 
 //Mis paginas
 // import SobreAurea from './pages/SobreAurea'
+
+const images = [
+  "./img1.png",
+  "./img2.png",
+  "./img3.png",
+  "./img4.png",
+  "./img5.png",
+  "./img6.png",
+];
 
 function App () {
   return (
     <>
           <BrowserRouter>
             <Navbar />
+              <Carousel images={images} interval={3000} />
             <Routes>
               <Route path='/' element= {<Hero />}></Route>
               <Route path='/SobreAurea'element={<SobreAurea />} />
+              
             </Routes>  
+            
             <Footer />
           </BrowserRouter>
     </>
